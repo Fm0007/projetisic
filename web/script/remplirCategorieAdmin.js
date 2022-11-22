3bb3faeac8e75f344b3c86cfc7a0aa32de2a655f
Fm0007 <fmed14.vlt@gmail.com>
2022-11-22 01:05:55 +0100

    Admin list categories marque product

---
+++ web/script/remplirCategorieAdmin.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+    $.ajax({  
+        url: "../ListAllProduct",
+        success: function (data, textStatus, jqXHR) {
+            remplirCategorie(data);
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log("erreur");
+        }
+        
+    });
+      $("#deleteProduct").click()(function () {
+        $.ajax({
+            url: "../DeleteProduct?id=" + $("#id").val(),
+            success: function (data, textStatus, jqXHR) {
+                remplirCategorie(data);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log("erreur");
+            }
+        });
+    });
+    
+    
+function remplirCategorie(data) {
+        var liste = $("#listproduit");
+        var option = "";
+        for (i = 0; i < data.length ; i++) {
+            option += "<tr>"
+                   +" <th scope='row'><input type='checkbox' /></th>"
+                    +" <td id='id' class='tm-product-name'>"+ data[i][0] +"</td>"
+                   +" <td>" + data[i][1] + "</td>"
+                   +" <td>" + data[i][2] + "</td>"
+                   + "<td>" + data[i][3] + "</td>"
+                  + " <td>"
+                   +  " <a id='deleteProduct'  class='tm-product-delete-link'>"
+                    +   " <i class='far fa-trash-alt tm-product-delete-icon'></i>"
+                  +   " </a>"
+                 +  " </td>"
+               +  " </tr>"
+                    
+        ;
+        }
+        liste.html(option);
+    }
+  
+      
+});
