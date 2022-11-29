8eb840d23e0ce2127a69363f04477dcb78a51a4c
Fm0007 <fmed14.vlt@gmail.com>
2022-11-29 10:48:33 +0100

    Coté Admin Commande

---
+++ src/java/test/Test1.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import controllers.SendMailTLS;
+import entities.Admin;
+import entities.Categorie;
+import entities.Client;
+import entities.Commande;
+import entities.LigneCommande;
+import entities.LigneCommandePK;
+import entities.Produit;
+import entities.User;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.EntityManager;
+import org.hibernate.Hibernate;
+import services.CategorieService;
+import services.CommandeService;
+import services.LigneCommandeService;
+import services.ProduitService;
+import services.UserService;
+import util.HibernateUtil;
+
+/**
+ *
+ * @author a
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        HibernateUtil.getSessionFactory().openSession();
+        UserService us = new UserService();
+        //us.create(new Client("fm@gmail.com","j","j","j","j" ,"123456"));
+       // us.create(new Admin("fm@gmail.com","123456"));
+        ProduitService ps = new ProduitService();
+        CommandeService cs = new CommandeService();
+        
+       LigneCommandeService ls = new LigneCommandeService();
+        System.out.println(cs.getPanier().getLignecommande().get(0).getProduit().getNom());
+      //  Client tmp = (Client) us.findByEmail("fm@gmail.com");
+       // Commande cat = cs.findById(1);
+     //  int nb = Integer.parseInt("1");
+        //ls.create(new LigneCommande(new LigneCommandePK(100,1),100,1));
+         
+              //  ls.delete(tmpp);
+                
+        //for(Produit c: cs.listAll()) System.out.println(c);
+    }
+
+}
---
+++ web/script/remplirCommandeAdmin.js
@@ -0,0 +1,57 @@
+$(document).ready(function () {
+                $.ajax({  
+               url: "../ListAllCategorie",
+               success: function (data, textStatus, jqXHR) {
+                   remplirCategorie(data);
+               },
+               error: function (jqXHR, textStatus, errorThrown) {
+                   console.log("erreur");
+               }
+
+               });
+               
+        
+       function deleteCategorie(id) {
+        $.ajax({
+            url: "../DeleteCaregorie?id=" + id,
+            success: function (data, textStatus, jqXHR) {     
+                remplirCategorie(data);     
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log("erreur");
+            }
+
+        });
+
+    }
+    
+    window.deleteCategorie=deleteCategorie;
+ 
+        
+    
+   
+    
+    
+function remplirCategorie(data) {
+        var liste = $("#listproduit");
+        var option = "";
+        for (i = 0; i < data.length ; i++) {
+            option += "<tr>"
+                   
+                    +" <td id='id' class='tm-product-name'>"+ data[i][0] +"</td>"
+                   +" <td>" + data[i][1] + "</td>"
+                  + " <td>"
+                   +  " <a onclick='deleteCategorie(" + data[i][0] +")'  class='tm-product-delete-link'>"
+                    +   " <i class='far fa-trash-alt tm-product-delete-icon'></i>"
+                  +   " </a>"
+                 +  " </td>"
+               +  " </tr>"
+                    
+        ;
+        }
+        liste.html(option);
+    }
+    
+  
+      
+});
